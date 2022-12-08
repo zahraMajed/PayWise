ee15393e6a1c1bbedd594db5243d62f2c77628b7
zahraMajed <zahra_m.alzawad@hotmail.com>
2022-12-08 13:29:52 +0300

    Initial Commit

---
+++ FinanceManagmentApp/FinanceManagmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceManagmentAppApp.swift
+//  FinanceManagmentApp
+//
+//  Created by Zahra Majed Alzawad on 14/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct FinanceManagmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
