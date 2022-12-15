2b10d8a2862feac8037213e448fae5dc67965fcf
reemaalsheddi <reemaalsheddii@gmail.com>
2022-12-15 15:31:03 +0300

    ..

--- FinanceManagmentApp/Model/User.swift
+++ FinanceManagmentApp/Model/User.swift
@@ -13,11 +13,11 @@ class User: ObservableObject {
     @Published var phoneNumber = ""
     @Published var dateOfBirth = Date.now
     @Published var nationalID = ""
-    var personalAccount : PersonalAccount
-    var liabilitiesAccount : LiabilitiesAccount
+    @Published var personalAccount  =  PersonalAccount(cardInfo: CardInfo(cardNumber: "", cvv: "", expDate: ""), cardSetting: CardSetting(cardLimit: 0, isCardLocked: false, isEcommerceTransDisabled: false), totalBalance: 0, thisMonthBudget: 0, thisMonthExpense: 0)
+    @Published var liabilitiesAccount = LiabilitiesAccount(cardInfo: CardInfo(cardNumber: "", cvv: "", expDate: "") , cardSetting: CardSetting(cardLimit: 0, isCardLocked: false, isEcommerceTransDisabled: false), liabilities: Liabilities(liabilityName: "", liabilityCost: 0), liabilitiesCost: 0, totalBalance: 0, thisMonthBudget: 0, thisMonthExpense: 0)
     
-    init(personalAccount: PersonalAccount, liabilitiesAccount: LiabilitiesAccount) {
+    /*init(personalAccount: PersonalAccount, liabilitiesAccount: LiabilitiesAccount) {
         self.personalAccount = personalAccount
         self.liabilitiesAccount = liabilitiesAccount
-    }
+    }*/
 }
