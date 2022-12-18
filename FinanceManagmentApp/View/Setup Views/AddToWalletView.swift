712262548c21f9b105288ed8176fee3624854e90
zahraMajed <zahra_m.alzawad@hotmail.com>
2022-12-18 15:09:11 +0300

    ..

---
+++ FinanceManagmentApp/View/Setup Views/AddToWalletView.swift
@@ -0,0 +1,20 @@
+//
+//  AddToWalletView.swift
+//  FinanceManagmentApp
+//
+//  Created by Zahra Majed Alzawad on 24/05/1444 AH.
+//
+
+import SwiftUI
+
+struct AddToWalletView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddToWalletView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddToWalletView()
+    }
+}
