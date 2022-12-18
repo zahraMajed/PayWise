2242e365d5e0a6f423ca63d10351ce34b4a4138a
zahraMajed <zahra_m.alzawad@hotmail.com>
2022-12-18 22:14:43 +0300

    ..

--- FinanceManagmentApp/View/Setup Views/Financial Information/RecurringLiabilitiesView.swift
+++ FinanceManagmentApp/View/Setup Views/Financial Information/RecurringLiabilitiesView.swift
@@ -21,74 +21,81 @@ struct RecurringLiabilitiesView: View {
     
     //MARK: body
     var body: some View {
+        VStack(){
+            Spacer()
+            ViewTitleDescription(viewTitle: "Recurring Liabilities", viewDescription: "We will store and send a varivarion code to it")
+            Spacer(minLength: 100)
+            VStack(spacing: 17){
+                CustomTextField(textFieldLabel: "Liability name", textFieldHint: "Car installments", isSwitch: false, isCurrancy: false, userInput: $liabilityName)
                 
-                    VStack(){
-                        Spacer()
-                        ViewTitleDescription(viewTitle: "Recurring Liabilities", viewDescription: "We will store and send a varivarion code to it")
-                        Spacer(minLength: 100)
-                        VStack(spacing: 17){
-                            CustomTextField(textFieldLabel: "Liability name", textFieldHint: "Car installments", isSwitch: false, isCurrancy: false, userInput: $liabilityName)
-                            
-                            CustomTextField(textFieldLabel: "Liability Cost", textFieldHint: "1000", isSwitch: false, isCurrancy: true, userInput: $liabilityCost)
-                                .keyboardType(.namePhonePad)
-                            
-                            LargeButton(text: "Add liability", isfilled: false) {
-                                userData.liabilitiesAccount.liabilities.append(Liabilities(liabilityName: liabilityName, liabilityCost: liabilityCost))
-                                if let liabCost = Int(liabilityCost) {
-                                    userData.liabilitiesAccount.liabilitiesCost += liabCost
-                                }
-                            }
-                        }
-                                            
-                        List{
-                            ForEach(userData.liabilitiesAccount.liabilities, id: \.self) { liability in
-                                listRow(liability: liability)
-                            }
-                            /*.onDelete(perform: deleteLiabilitie)
-                             .onMove(perform: moveLiabilitie)
-                             .onLongPressGesture{
-                             withAnimation{
-                             self.editinglidt = true
-                             }
-                             }
-                             .environment(\.editMode, editinglidt ?.constant(.active): .constant(.inactive))*/
-                            .listRowSeparator(.hidden)
-                        }
-                        .padding(.bottom, 17.0)
-                        .frame(maxHeight: 200)
-                        .listStyle(.plain)
-                        if userData.liabilitiesAccount.liabilitiesCost > 0 {
-                            HStack{
-                                Text("Total liabilities Cost:")
-                                    .font(.headline)
-                                Text("\(userData.liabilitiesAccount.liabilitiesCost)")
-                                Spacer()
-                            }
-                            .padding(.leading, 17)
-                        }
-                        Spacer()
-                        LargeButton(text: "Continue", isfilled: true) {
-                            showNextView = true
-                        }
-                        .padding(.bottom, 20).labelsHidden()
-                        NavigationLink(destination: MoneyGrowthGoal(), isActive: $showNextView) {
-                        }
+                CustomTextField(textFieldLabel: "Liability Cost", textFieldHint: "1000", isSwitch: false, isCurrancy: true, userInput: $liabilityCost)
+                    .keyboardType(.namePhonePad)
+                
+                LargeButton(text: "Add liability", isfilled: false) {
+                    userData.liabilitiesAccount.liabilities.append(Liabilities(liabilityName: liabilityName, liabilityCost: liabilityCost))
+                    if let liabCost = Int(liabilityCost) {
+                        userData.liabilitiesAccount.liabilitiesCost += liabCost
                     }
+                }
+            }
+                                
+            List{
+                ForEach(userData.liabilitiesAccount.liabilities, id: \.self) { liability in
+                    listRow(liability: liability)
+                }
+                .onDelete(perform: deleteLiabilitie)
+                 .onMove(perform: moveLiabilitie)
+                 .onLongPressGesture{
+                 withAnimation{
+                 self.editinglidt = true
+                 }
+                 }
+                 .environment(\.editMode, editinglidt ?.constant(.active): .constant(.inactive))
+                .listRowSeparator(.hidden)
+            }
+            .padding(.bottom, 17.0)
+            .frame(maxHeight: 200)
+            .listStyle(.plain)
+            if userData.liabilitiesAccount.liabilitiesCost > 0 {
+                HStack{
+                    Text("Total liabilities Cost:")
+                        .font(.headline)
+                    Text("\(userData.liabilitiesAccount.liabilitiesCost)")
+                    Spacer()
+                }
+                .padding(.leading, 17)
+            }
+            Spacer()
+            LargeButton(text: "Continue", isfilled: true) {
+                showNextView = true
+            }
+            .padding(.bottom, 20).labelsHidden()
+            NavigationLink(destination: MoneyGrowthGoal(), isActive: $showNextView) {
+            }
+        }
     }
     //MARK: function
     func division(){
           guard let montlyIncom = Int(userData.monthlyIncom) else {
               return
           }
-        let budgetPerAcc = (montlyIncom/3)
-          userData.personalAccount.totalBalance += budgetPerAcc
-          userData.liabilitiesAccount.totalBalance += budgetPerAcc
-          userData.businessAccount.totalBalance += budgetPerAcc
-          
-          userData.personalAccount.thisMonthBudget = budgetPerAcc
-          userData.liabilitiesAccount.thisMonthBudget = budgetPerAcc
-          userData.businessAccount.thisMonthBudget = budgetPerAcc
-          
+        if userData.liabilitiesAccount.liabilitiesCost > (montlyIncom/3) {
+            userData.businessAccount.totalBalance += (montlyIncom/3)
+            userData.personalAccount.totalBalance += montlyIncom - (montlyIncom/3) - userData.liabilitiesAccount.liabilitiesCost
+            userData.liabilitiesAccount.totalBalance += userData.liabilitiesAccount.liabilitiesCost
+            //what if liabilitiesCost much more than monthly incom
+            // show alert here as a tips
+            
+        } else if userData.liabilitiesAccount.liabilitiesCost <= (montlyIncom/3) {
+            let budgetPerAcc = (montlyIncom/3)
+            userData.personalAccount.totalBalance += budgetPerAcc
+            userData.liabilitiesAccount.totalBalance += budgetPerAcc
+            userData.businessAccount.totalBalance += budgetPerAcc
+            
+            userData.personalAccount.thisMonthBudget = budgetPerAcc
+            userData.liabilitiesAccount.thisMonthBudget = budgetPerAcc
+            userData.businessAccount.thisMonthBudget = budgetPerAcc
+        }
       }
       
       func deleteLiabilitie(indexSet : IndexSet){
