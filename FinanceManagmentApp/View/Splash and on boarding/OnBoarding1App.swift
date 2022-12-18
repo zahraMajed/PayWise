3c9a53cf8b3430a5a8a99d1368ee8e8b8281c999
Shaden03 <116809090+Shaden03@users.noreply.github.com>
2022-12-18 03:31:56 +0300

    Add files via upload

---
+++ FinanceManagmentApp/View/Splash and on boarding/OnBoarding1App.swift
@@ -0,0 +1,19 @@
+//
+//  OnBoarding1App.swift
+//  OnBoarding1
+//
+//  Created by Shaden on 19/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct OnBoarding1App: App {
+    var body: some Scene {
+        WindowGroup {
+           SplashScreen()
+        }
+    }
+}
+ 
+// splashscreen instead of content view so the splash screen will appear
