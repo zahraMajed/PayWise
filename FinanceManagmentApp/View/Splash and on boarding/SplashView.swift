c954d2645ab5505f87bd1bec1cd34c3cecc18394
zahraMajed <zahra_m.alzawad@hotmail.com>
2022-12-20 12:36:14 +0300

    ..

--- FinanceManagmentApp/View/Splash and on boarding/SplashView.swift
+++ FinanceManagmentApp/View/Splash and on boarding/SplashView.swift
@@ -67,6 +67,7 @@ struct SplashView: View {
 struct SplashView_Previews: PreviewProvider {
     static var previews: some View {
         SplashView()
+            .environmentObject(User())
     }
 }
 
