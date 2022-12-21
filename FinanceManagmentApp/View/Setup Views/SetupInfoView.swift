7215419b40270ecf55b9f0ea622f49386d38734e
zahraMajed <zahra_m.alzawad@hotmail.com>
2022-12-21 13:18:30 +0300

    ..

--- FinanceManagmentApp/View/Setup Views/SetupInfoView.swift
+++ FinanceManagmentApp/View/Setup Views/SetupInfoView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import FLAnimatedImage
 
 struct SetupInfoView: View {
     
@@ -79,9 +80,6 @@ struct SetupInfoView_Previews: PreviewProvider {
 }
 
 
-
-
-
 struct onboarding1: View {
     @Binding var shouldshowonboarding: Bool
     var body: some View {
@@ -110,8 +108,6 @@ struct onboarding1: View {
     }
 }
 
-
-
 struct PageView: View {
     let title: String
     let subtitle: String
@@ -122,15 +118,14 @@ struct PageView: View {
     var body: some View {
         ZStack{
             
-            Image(imageName)
+            /*Image(imageName)
                 .resizable()
-                .ignoresSafeArea()
+                .ignoresSafeArea()*/
             
             VStack{
                 
-                
-                
-                Spacer(minLength:500)
+               // Spacer(minLength:500)
+                GIFView(type: .name(""))
                 
                 
                 Text(title)
@@ -189,3 +184,72 @@ struct PageView: View {
         
         
     }
+
+struct GIFView: UIViewRepresentable {
+  private var type: URLType
+
+  init(type: URLType) {
+    self.type = type
+  }
+
+  private let imageView: FLAnimatedImageView = {
+    let imageView = FLAnimatedImageView()
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    imageView.layer.cornerRadius = 24
+    imageView.layer.masksToBounds = true
+    return imageView
+  }()
+
+  private let activityIndicator: UIActivityIndicatorView = {
+    let activityIndicator = UIActivityIndicatorView()
+    activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+    return activityIndicator
+  }()
+}
+
+extension GIFView {
+  func makeUIView(context: Context) -> UIView {
+    let view = UIView(frame: .zero)
+
+    view.addSubview(activityIndicator)
+    view.addSubview(imageView)
+
+    imageView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+    imageView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+
+    activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+    return view
+  }
+
+  func updateUIView(_ uiView: UIView, context: Context) {
+    activityIndicator.startAnimating()
+    guard let url = type.url else { return }
+
+    DispatchQueue.global().async {
+      if let data = try? Data(contentsOf: url) {
+        let image = FLAnimatedImage(animatedGIFData: data)
+
+        DispatchQueue.main.async {
+          activityIndicator.stopAnimating()
+          imageView.animatedImage = image
+        }
+      }
+    }
+  }
+}
+
+enum URLType {
+  case name(String)
+  case url(URL)
+
+  var url: URL? {
+    switch self {
+      case .name(let name):
+        return Bundle.main.url(forResource: name, withExtension: "gif")
+      case .url(let remoteURL):
+        return remoteURL
+    }
+  }
+}
